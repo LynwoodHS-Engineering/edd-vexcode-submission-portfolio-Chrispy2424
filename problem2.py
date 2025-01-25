78abec7f78aa4a4b12d79505542ee98f98720dfa
Chrispy2424 <chrpin0107@stu.mylusd.org>
2025-01-25 08:34:32 -0800

    problem2.py

---
+++ problem2.py
@@ -0,0 +1,14 @@
+code:
+while True:
+    pass
+    if limit_switch_b.pressing() and limit_switch_f.pressing():
+
+
+        pass
+        led_yellow.on()
+        led_green.off()
+
+    else:
+        pass 
+        led_green.on()
+        led_yellow.off()
